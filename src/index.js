a3b2dcdc5ee977ffd4e4f5182ba5ad3b4202b379
Maximilian Heidenreich <github@maximilian-heidenreich.de>
2020-10-25 12:38:15 +0100

    Adds snipp api routs & Swagger API docs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 const { Pool } = require('pg')
+const { generateId } = require('./utils')
 
 // Create pool.
 const pool = new Pool({
@@ -16,9 +19,58 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
-const getBooks = async (req, res) => {
+// GET /snipp
+const getSnipps = async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM snipps')
+        const results = { 'results': (result) ? result.rows : null}
+        res.json({ err: false, data: results })
+        client.release()
+    } catch (err) {
+        console.error(err)
+        res.json({ err: true, data: err })
+    }
+}
+
+// POST /snipp
+const addSnipp = async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('INSERT INTO snipps (ID, name, lang, ownerPin, content) VALUES  ($1, $2, $3, $4, $5)', [
+            generateId(6),
+            req.body.name,
+            req.body.lang,
+            req.body.ownerPin,
+            req.body.content,
+        ])
+        res.json({ err: false, data: result })
+        client.release()
+    } catch (err) {
+        console.error(err)
+        res.json({ err: true, data: err })
+    }
+}
+
+// GET /snipp/:snippID
+const getSnipp = async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM snipps WHERE ID="$1"', [req.params.snippID])
+        const results = { 'results': (result) ? result.rows[0] : null}
+        res.json({ err: false, data: results })
+        client.release()
+    } catch (err) {
+        console.error(err)
+        res.json({ err: true, data: err })
+    }
+}
+
+// POST /snipp/:snippID
+const updateSnipp = async (req, res) => {
     try {
         const client = await pool.connect()
         const result = await client.query('SELECT * FROM snipps')
@@ -32,14 +84,14 @@ const getBooks = async (req, res) => {
 }
 
 // Add routes.
-app.route('/books')
-    // GET endpoint
-    .get(getBooks)
-    // POST endpoint
-    //.post(addBook)
+app.route('/snipp')
+    .get(getSnipps)
+    .post(addSnipp)
+app.route('/snipp/:snippID')
+    .get(getSnipp)
+    .post(updateSnipp)
 
 // Start server.
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
--- src/init.sql
+++ src/init.sql
@@ -1,8 +1,10 @@
 CREATE TABLE snipps (
-    ID SERIAL PRIMARY KEY,
-    author VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL
+    ID uuid PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    lang VARCHAR(255) NOT NULL,
+    ownerPin VARCHAR(255) NOT NULL,
+    content VARCHAR(255) NOT NULL
 );
 
-INSERT INTO snipps (author, title)
-VALUES  ('J.K. Rowling', 'Harry Potter');
+INSERT INTO snipps (ID, name, lang, ownerPin, content)
+VALUES  ('c7f41315-bc8f-4918-8251-23db7db0ddc4', 'Test SNipp', 'js', '000-000', 'Geklo World');
